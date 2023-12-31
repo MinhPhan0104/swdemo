1ec6e0fe5a44b58c8f24983fd236f55e92ce7884
MinhPhan0104 <87423834+MinhPhan0104@users.noreply.github.com>
2023-12-31 21:34:00 +0700

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import TailwindCSS from "./TailwindCSS.svelte";
+  import TailwindDemo from "./TailwindDemo.svelte";
+</script>
+
+<TailwindDemo />
+
+<!-- DON'T DELETE IT OR TAILWIND WILL NOT WORK WORK -->
+<TailwindCSS />
---
+++ src/TailwindDemo.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+</script>
+
+<div class="text-center">
+  <div class="inside-style-tag">Style inside style tag with postcss</div>
+  <div class="text-5xl text-pink">Style directly with class</div>
+</div>
+
+<style lang="postcss">
+  .inside-style-tag {
+    @apply text-[blue];
+  }
+</style>
